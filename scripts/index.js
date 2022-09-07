a68f7b1a25e755392cb87b407218728d5d9ef60c
Denys Vasylyev <denvasden@gmail.com>
2022-09-07 17:34:16 +0300

    Verify lint-staged

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,19 @@
 const axios = require("axios").default;
-const { parserText, writeDataToJSONFiles } = require("./utilities");
+const { parserText, writeDataToJSONFiles, parserFIFA, benchmark } = require("./utilities");
 
-const dataFIFA = {
-   countries: [],
-   leagues: [],
-   teams: [],
-};
-
-const URLFIFARu = "https://www.ea.com/ru-ru/games/fifa/fifa-22/news/fifa-22-all-leagues-clubs-teams-list";
 const URLFIFA = "https://www.ea.com/games/fifa/fifa-22/news/fifa-22-all-leagues-clubs-teams-list?setLocale=en-us"
 
 async function getAndWriteDataFifa(URL) {
 
-   const response = await axios(URL);   
+   const response = await axios(URL);
 
-   parserText(response.data, dataFIFA);
-   writeDataToJSONFiles("../data", dataFIFA);
+   // parserText(response.data);
+   // writeDataToJSONFiles("../data", dataFIFA);
+   console.log(benchmark(parserText, response.data));
+   console.log(benchmark(parserFIFA, response.data));
+   
+    
+   
 }
 
 getAndWriteDataFifa(URLFIFA);
