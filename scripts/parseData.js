f5877353b184ce060850f76b99267fdd86287304
docenkoserg <serg.docenko0@gmail.com>
2022-09-19 19:59:07 +0300

    Refactoring done

--- scripts/parseData.js
+++ scripts/parseData.js
@@ -1,116 +1,94 @@
-const {
-  getOneAbstraction,
-  parseCountry,
-  parseLeague,
-  parseTeam,
-} = require("./utilities");
 
-const COUNTRIES_WITHOUT_LEAGUE = [
-  "WOMEN'S NATIONAL",
-  "REST OF WORLD",
-  "MEN'S NATIONAL",
-];
 
-function parseText(
-  textData,
-  substrStart = "ARGENTINA",
-  substrEnd = "Wilstermann"
-) {
-  const dataFIFA = {
-    countries: [],
-    leagues: [],
-    teams: [],
-  };
+const { getOneAbstraction, parseCountry, parseLeague, parseTeam } = require("./utilities")
 
-  let indexStartIncludes = textData.indexOf(substrStart);
-  let indexEnd = textData.lastIndexOf(substrEnd);
-  let coreText;
-  let arrayCountriesAndTeams;
+const COUNTRIES_WITHOUT_LEAGUE = ["WOMEN'S NATIONAL", "REST OF WORLD", "MEN'S NATIONAL"]
 
-  indexEnd = textData.indexOf("</p>", indexEnd);
+function parseText(textData, substrStart = "<h2>ARGENTINA</h2", substrEnd = "<p>Wilstermann</p>") {
 
-  coreText = textData.slice(indexStartIncludes, indexEnd) + "</p>";
+   const dataFIFA = {
+      countries: [],
+      leagues: [],
+      teams: [],
+   };
 
-  arrayCountriesAndTeams = coreText.split("<h2>");
+   let indexStartIncludes = textData.indexOf(substrStart);
+   let indexEnd = textData.lastIndexOf(substrEnd);
+   let coreText;
+   let arrayCountriesAndTeams;
 
-  arrayCountriesAndTeams.forEach((textBlock) =>
-    parseBlock(textBlock, dataFIFA)
-  );
+   indexEnd = textData.indexOf("</p>", indexEnd);
 
-  return dataFIFA;
+   coreText = textData.slice(indexStartIncludes, indexEnd) + "</p>";
+
+   arrayCountriesAndTeams = coreText.split("<h2>");
+
+   arrayCountriesAndTeams.forEach(textBlock => parseBlock(textBlock, dataFIFA));
+
+   return dataFIFA;
 }
 
 function parseBlock(textBlock, dataFIFA) {
-  let contriesLeaguesAndTeams = textBlock.split("<p>");
 
-  contriesLeaguesAndTeams.forEach((line) => {
-    const country = parseCountry(line);
-    const league = parseLeague(line);
-    const team = parseTeam(line);
+   let contriesLeaguesAndTeams = textBlock.split("<p>");
 
-    country && dataFIFA.countries.push(country);
-    league &&
-      dataFIFA.leagues.push({
-        country: dataFIFA.countries[dataFIFA.countries.length - 1],
-        league: league,
-      });
-    team &&
-      dataFIFA.teams.push({
-        ...dataFIFA.leagues[dataFIFA.leagues.length - 1],
-        team: team,
-      });
+   contriesLeaguesAndTeams.forEach(line => {
 
-    if (COUNTRIES_WITHOUT_LEAGUE.includes(country))
-      dataFIFA.leagues.push({ country: country, league: country });
-  });
+      const country = parseCountry(line);
+      const league = parseLeague(line);
+      const team = parseTeam(line)
+
+      country && dataFIFA.countries.push(country);
+      league && dataFIFA.leagues.push({ "country": dataFIFA.countries[dataFIFA.countries.length - 1], "league": league });
+      team && dataFIFA.teams.push({ ...dataFIFA.leagues[dataFIFA.leagues.length - 1], "team": team });
+
+      if (COUNTRIES_WITHOUT_LEAGUE.includes(country))
+         dataFIFA.leagues.push({ "country": country, "league": country });
+   })
 }
 
 // -----------------------------------------------------------------------------------------------//
 
-function parseFIFA(
-  textData,
-  substrStart = "<h2>ARGENTINA",
-  substrEnd = "Wilstermann</p>"
-) {
-  const indexIncludesSubstrStart = textData.indexOf(substrStart);
-  const indexIncludesSubstrEnd = textData.lastIndexOf(substrEnd);
+function parseFIFA(textData, substrStart = "<h2>ARGENTINA</h2>", substrEnd = "<p>Wilstermann</p>") {
 
-  const coreText =
-    textData.slice(indexIncludesSubstrStart, indexIncludesSubstrEnd) +
-    substrEnd;
+   const indexIncludesSubstrStart = textData.indexOf(substrStart);
+   const indexIncludesSubstrEnd = textData.lastIndexOf(substrEnd);
 
-  return parseCore(coreText);
+   const coreText = textData.slice(indexIncludesSubstrStart, indexIncludesSubstrEnd) + substrEnd;
+
+   return parseCore(coreText);
 }
 
 function parseCore(coreText) {
-  const dataFIFA = {
-    countries: [],
-    leagues: [],
-    teams: [],
-  };
 
-  const regExp = /<h2>.*<\/h2>|<p>.*<\/p>/g;
-  let abstraction;
+   const dataFIFA = {
+      countries: [],
+      leagues: [],
+      teams: [],
+   };
 
-  while ((abstraction = regExp.exec(coreText))) {
-    const lastCountries = dataFIFA.countries[dataFIFA.countries.length - 1];
-    const lastLegaues = dataFIFA.leagues[dataFIFA.leagues.length - 1];
+   const regExp = /<h2>.*<\/h2>|<p>.*<\/p>/g;
+   let abstraction;
 
-    const country = getOneAbstraction(abstraction[0], "h2");
-    const league = getOneAbstraction(abstraction[0], "b");
-    const team = getOneAbstraction(abstraction[0], "p");
+   while (abstraction = regExp.exec(coreText)) {
 
-    country && dataFIFA.countries.push(country);
-    league && dataFIFA.leagues.push({ country: lastCountries, league: league });
-    team && dataFIFA.teams.push({ ...lastLegaues, team: team });
+      const lastCountries = dataFIFA.countries[dataFIFA.countries.length - 1];
+      const lastLegaues = dataFIFA.leagues[dataFIFA.leagues.length - 1];
 
-    if (COUNTRIES_WITHOUT_LEAGUE.includes(country))
-      dataFIFA.leagues.push({ country: country, league: country });
-  }
-  return dataFIFA;
+      const country = getOneAbstraction(abstraction[0], "h2");
+      const league = getOneAbstraction(abstraction[0], "b");
+      const team = getOneAbstraction(abstraction[0], "p");
+
+      country && dataFIFA.countries.push(country);
+      league && dataFIFA.leagues.push({ "country": lastCountries, "league": league });
+      team && dataFIFA.teams.push({ ...lastLegaues, "team": team });
+
+      if (COUNTRIES_WITHOUT_LEAGUE.includes(country))
+         dataFIFA.leagues.push({ "country": country, "league": country });
+   }
+   return dataFIFA;
 }
 
 module.exports = {
-  parseText,
-  parseFIFA,
-};
+   parseText, parseFIFA
+}
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -33,29 +33,36 @@ function writeDataToJSONFiles(path, dataObject) {
 }
 
 function writeToJSONFile(path, fileName, data) {
-  fs.mkdir(path, (error) => {
-    if (!error || error.code === "EEXIST") {
-      fs.writeFile(`${path}/${fileName}`, JSON.stringify(data), (error) =>
-        error ? console.log(`Ошибка при создании файла: ${fileName}`) : null
-      );
-    } else console.log("Ошибка при создании каталога: /data");
-  });
+
+   fs.mkdir(path, error => {
+      if (!error || error.code === "EEXIST") {
+
+         fs.writeFile(`${path}/${fileName}`, JSON.stringify(data),
+            error => error ? console.log(`Ошибка при создании файла: ${fileName}`) : null);
+      }
+      else console.log(("Ошибка при создании каталога: /data"));
+   });
 }
 
-function parseCountry(string) {
-  if (string.includes("</h2>")) {
-    return sanitize(string);
-  }
+function parseCountry(string){
+
+   if (string.includes("</h2>")) {
+      return sanitize(string);
+   }   
+
 }
 function parseLeague(string) {
-  if (string.includes("</b2>")) {
-    return sanitize(string);
-  }
+
+   if (string.includes("</b2>")) {
+      return sanitize(string);
+   }
+
 }
-function parseTeam(string) {
-  if (string.includes("</p>") && !string.includes("</b>")) {
-    return sanitize(string);
-  }
+function parseTeam(string){
+   
+   if (string.includes("</p>") && !string.includes("</b>")) {
+      return sanitize(string);
+   }
 }
 
 module.exports = {
