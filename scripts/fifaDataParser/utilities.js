ef95f2608849fda63aa4bd1d96ec0164cde6e72a
Denys Vasylyev <denvasden@gmail.com>
2022-10-04 13:07:47 +0300

    Remove redundant sources and not optimal solution

--- scripts/fifaDataParser/utilities.js
+++ scripts/fifaDataParser/utilities.js
@@ -2,43 +2,6 @@ const fs = require("fs/promises");
 
 const { COUNTRY_MARKUP, LEAGUE_MARKUP, TEAM_MARKUP } = require("./constants");
 
-function benchmark(numberIterations = 10, func, ...funcArgs) {
-  const now = Date.now();
-
-  for (let i = 0; i < numberIterations; i++) func(...funcArgs);
-
-  return Date.now() - now;
-}
-
-function sanitize(string = "") {
-  const controlCharacters = "\\r?\\n";
-  const htmlMarkup = "<\\/?\\w*>";
-  const specialCharacters = "[(,&*].*";
-  const specialStrings = "Please note.*";
-
-  return string.replace(
-    new RegExp(
-      `${controlCharacters}|${htmlMarkup}|${specialCharacters}|${specialStrings}`,
-      "gi"
-    ),
-    ""
-  );
-}
-
-function getSanitizedString(string, tag, unincludesTag = "<b>") {
-  // Unique case, works only with non-standard command markup
-  const uniqueMarkupTeam = string.match(/(.*<\/p>)|<p>.*<br>/);
-  if (uniqueMarkupTeam && tag === "p") return sanitize(uniqueMarkupTeam[0]);
-
-  // Standard case
-  const regExp = new RegExp(`<${tag}>.*</${tag}>`);
-  const abstraction = string.match(regExp);
-
-  if (abstraction && abstraction[0].includes(`>${unincludesTag}`)) return null;
-
-  return abstraction ? sanitize(abstraction[0]) : null;
-}
-
 async function writeData(path, data) {
   try {
     await fs.rm(path, { recursive: true });
@@ -58,6 +21,21 @@ async function writeFile(path, data) {
   } catch (error) {
     console.error(error);
   }
+}
+
+function sanitize(string = "") {
+  const controlCharacters = "\\r?\\n";
+  const htmlMarkup = "<\\/?\\w*>";
+  const specialCharacters = "[(,&*].*";
+  const specialStrings = "Please note.*";
+
+  return string.replace(
+    new RegExp(
+      `${controlCharacters}|${htmlMarkup}|${specialCharacters}|${specialStrings}`,
+      "gi"
+    ),
+    ""
+  );
 }
 
 function parseCountry(string = "") {
@@ -93,16 +71,9 @@ function parseTeam(string = "") {
   return sanitize(match[0]);
 }
 
-function fixMarkupUniqueCommand(string) {
-  return string.replace("<br>", "</p><p>");
-}
-
 module.exports = {
-  getSanitizedString,
-  benchmark,
   parseCountry,
   parseLeague,
   parseTeam,
-  fixMarkupUniqueCommand,
   writeData,
 };
